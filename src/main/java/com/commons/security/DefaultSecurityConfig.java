dd93468c5d62f57c7bf9b5f2a7d91999fc9aca2a
mulesoftenthusiast <mulesoftenthusiast@gmail.com>
2025-07-04 13:11:56 -0400

    bug fixes

--- src/main/java/com/commons/security/DefaultSecurityConfig.java
+++ src/main/java/com/commons/security/DefaultSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.commons.security;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,14 +8,21 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 public class DefaultSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+    	System.out.print("Loadedddddddddddddddddd!!!!!!!!!!!!!!!!!!!!!");
         http
+            .csrf().disable() // Important: disable CSRF for non-browser POSTs
+
             .authorizeHttpRequests()
-            .requestMatchers("/api/v1/customers").permitAll() // Public registration
+            .requestMatchers("/.well-known/jwks.json","/api/v1/register","/api/v1/customer/register","/api/v1/auth/login",
+                    "/oauth/token").permitAll()
             .requestMatchers("/health").permitAll() // Optional health check
                 .anyRequest().authenticated()
             .and()
@@ -34,5 +42,11 @@ public class DefaultSecurityConfig {
         JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
         return jwtConverter;
+    }
+    
+    
+    @PostConstruct
+    public void init() {
+        System.out.println("✅ DefaultSecurityConfig loaded in context");
     }
 }
